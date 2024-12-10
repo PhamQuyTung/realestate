3d0202ba43487491171b3adfcbfdc8e28eca6a5b
PhamQuyTung <pqthp18903@gmail.com>
2024-12-11 01:02:40 +0700

    B96: Xây dựng UI phần Header #1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,11 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { PhoneIcon } from '~/components/Icon';
+import Tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css'; // optional for styling
 
 const cx = classNames.bind(styles);
 
@@ -11,48 +16,57 @@ function Header() {
                 <div className={cx('row')}>
                     <div className={cx('col', 'l-12', 'm-12', 'c-12')}>
                         <div className={cx('header-body')}>
-                            <div className={cx('logo')}>
-                                <h1>LOGO</h1>
-                            </div>
+                            {/* LOGO */}
+                            <Link to="/" className={cx('logo')}>
+                                {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                                <img src="https://themesflat.co/html/proty/images/logo/logo@2x.png" />
+                            </Link>
+
+                            {/* MENU */}
                             <div className={cx('menu')}>
                                 <ul className={cx('list')}>
                                     <li>
                                         <Link to="/" className={cx('menu-item')} href="#">
-                                            Home
+                                            <span>Home</span>
                                         </Link>
                                     </li>
                                     <li>
                                         <Link to="/Property" className={cx('menu-item')} href="#">
-                                            Property
+                                            <span>Property</span>
                                         </Link>
                                     </li>
                                     <li>
-                                        <span className={cx('menu-item')}>More</span>
+                                        <span className={cx('menu-item')}>
+                                            More
+                                            <FontAwesomeIcon className={cx('icon')} icon={faAngleDown} />
+                                        </span>
                                     </li>
                                     <li>
                                         <Link to="/Blog" className={cx('menu-item')} href="#">
-                                            Blog
+                                            <span>Blog</span>
                                         </Link>
                                     </li>
                                     <li>
                                         <Link to="/Contact" className={cx('menu-item')} href="#">
-                                            Contact
+                                            <span>Contact</span>
                                         </Link>
                                     </li>
                                 </ul>
                             </div>
+
                             {/* Actions */}
                             <div>
                                 <ul className={cx('actions')}>
                                     <li className={cx('actions-item')}>
-                                        <span>action1</span>
+                                        <PhoneIcon className={cx('icon')} />
+                                        <span className={cx('actions-item_phone')}>(603) 555-0123</span>
                                     </li>
                                     <li className={cx('actions-item')}>
-                                        <span>action2</span>
+                                        <span className={cx('actions-item')}>login</span>
                                     </li>
-                                    <li className={cx('actions-item')}>
-                                        <span>action3</span>
-                                    </li>
+                                    {/* <li className={cx('actions-item')}>
+                                        <span className={cx('actions-item')}>action3</span>
+                                    </li> */}
                                 </ul>
                             </div>
                         </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
+            <h1 style={{color: 'white'}}>Welcome to the Home Page</h1>
         </div> 
     )
 }
