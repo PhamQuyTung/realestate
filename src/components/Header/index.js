8af4b4b4fb783cd8a896882fe46bac7973eb2cda
PhamQuyTung <pqthp18903@gmail.com>
2024-12-16 14:36:30 +0700

    B96(Bonus): Xây dựng UI phần Header #1C

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,76 @@ import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faEllipsis, faEllipsisVertical, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { ElipsIcon } from '~/components/Icon';
+import {
+    DashboardsIcon,
+    ProfileIcon,
+    PackageIcon,
+    FavoriteIcon,
+    FileSaveIcon,
+    ReviewIcon,
+    PropertiesIcon,
+    PropertyIcon,
+    LogoutIcon,
+} from '~/components/Icon';
 import Tippy from '@tippyjs/react/headless'; // different import path!
 // import 'tippy.js/dist/tippy.css'; // optional for styling
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    // menu items user
+    const userMenuItems = [
+        {
+            icon: <DashboardsIcon />,
+            title: 'Dashboards',
+            to: '/dashboard',
+        },
+        {
+            icon: <ProfileIcon />,
+            title: 'My Profile',
+            to: '/myprofile',
+        },
+        {
+            icon: <PackageIcon />,
+            title: 'My Package',
+            to: '/mypackage',
+        },
+        {
+            icon: <FavoriteIcon />,
+            title: 'My Favorites',
+            to: '/myfavorites',
+        },
+        {
+            icon: <FileSaveIcon />,
+            title: 'My save searches',
+            to: '/mysavesearches',
+        },
+        {
+            icon: <ReviewIcon />,
+            title: 'Reviews',
+            to: '/myreviews',
+        },
+        {
+            icon: <PropertiesIcon />,
+            title: 'My Properties',
+            to: '/myproperties',
+        },
+        {
+            icon: <PropertyIcon />,
+            title: 'Add Property',
+            to: '/addproperty',
+        },
+        {
+            icon: <LogoutIcon />,
+            title: 'Log out',
+            to: '/',
+        },
+    ];
+
+    //
+    const currentUser = true;
+
+    // render jsx
     return (
         <div className={cx('header-top')}>
             <div className={cx('grid', 'wide')}>
@@ -72,9 +135,47 @@ function Header() {
                                         <FontAwesomeIcon icon={faPhone} className={cx('icon-phone')} />
                                         <span className={cx('actions-item_phone')}>(603) 555-0123</span>
                                     </li>
-                                    <li className={cx('actions-item')}>
-                                        <button className={cx('actions-item__loginBtn')}>Login</button>
-                                    </li>
+
+                                    {currentUser ? (
+                                        <li className={cx('actions-item')}>
+                                            <Tippy
+                                                // visible
+                                                interactive
+                                                delay={[0, 300]}
+                                                placement="bottom-end"
+                                                render={(attrs) => (
+                                                    <ul className={cx('box-menu__list')} tabIndex="-1" {...attrs}>
+                                                        {userMenuItems.map((item, index) => {
+                                                            return (
+                                                                <Link
+                                                                    key={index}
+                                                                    to={item.to}
+                                                                    className={cx('box-menu__item-link')}
+                                                                >
+                                                                    {item.icon}
+                                                                    <span className={cx('box-menu__item-title')}>
+                                                                        {item.title}
+                                                                    </span>
+                                                                </Link>
+                                                            );
+                                                        })}
+                                                    </ul>
+                                                )}
+                                            >
+                                                <div className={cx('current-user')}>
+                                                    <img
+                                                        src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                                                        alt="avatar"
+                                                    />
+                                                </div>
+                                            </Tippy>
+                                        </li>
+                                    ) : (
+                                        <li className={cx('actions-item')}>
+                                            <button className={cx('actions-item__loginBtn')}>Login</button>
+                                        </li>
+                                    )}
+
                                     <li className={cx('actions-item')}>
                                         <Tippy
                                             // visible
