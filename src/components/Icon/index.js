fa39818c047bd32ad71fd38818a63f7c321ae79d
PhamQuyTung <pqthp18903@gmail.com>
2024-12-17 14:08:10 +0700

    RealEstate Content: Xây dựng phần Content-TF #1

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,19 +1,27 @@
-export const ElipsIcon = ({ width = '2rem', height = '2rem', className }) => (
-    <svg
-        className={className}
-        width={width}
-        height={height}
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M24 4C26.2091 4 28 5.79086 28 8C28 10.2091 26.2091 12 24 12C21.7909 12 20 10.2091 20 8C20 5.79086 21.7909 4 24 4ZM24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24C20 21.7909 21.7909 20 24 20ZM24 36C26.2091 36 28 37.7909 28 40C28 42.2091 26.2091 44 24 44C21.7909 44 20 42.2091 20 40C20 37.7909 21.7909 36 24 36Z"
-        ></path>
-    </svg>
-);
+// export const BuildingIcon = ({ width = '8rem', height = '8rem', className }) => (
+//     <svg
+//         className={className}
+//         width={width}
+//         height={height}
+//         viewBox="0 0 80 80"
+//         fill="none"
+//         xmlns="http://www.w3.org/8000/svg"
+//         stroke="currentColor"
+//         stroke-width="2"
+//         stroke-linecap="round"
+//         stroke-linejoin="round"
+//     >
+//         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+//         <path d="M3 21l18 0" />
+//         <path d="M9 8l1 0" />
+//         <path d="M9 12l1 0" />
+//         <path d="M9 16l1 0" />
+//         <path d="M14 8l1 0" />
+//         <path d="M14 12l1 0" />
+//         <path d="M14 16l1 0" />
+//         <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16" />
+//     </svg>
+// );
 
 export const DashboardsIcon = ({ width = '2rem', height = '2rem', className, color = 'white' }) => (
     <svg
