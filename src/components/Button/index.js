865f13bb027509efc04ee707e55332e1c2359899
PhamQuyTung <pqthp18903@gmail.com>
2024-12-19 13:08:04 +0700

    RealEstate Content: Xây dựng UI phần Content-Discover #2 (Hoàn Thành)

---
+++ src/components/Button/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+
+// Sử dụng classnames để dễ quản lý className
+const cx = classNames.bind(styles);
+
+const Button = ({
+    children,
+    size = 'medium', // small, medium, large
+    variant = 'primary', // primary, secondary, outline, text
+    onClick,
+    disabled = false,
+    ...props
+}) => {
+    const classes = cx('button', {
+        [size]: size,
+        [variant]: variant,
+        disabled: disabled,
+    });
+
+    return (
+        <button className={classes} onClick={onClick} disabled={disabled} {...props}>
+            {children}
+        </button>
+    );
+};
+
+// Kiểm tra kiểu dữ liệu và giá trị mặc định
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    variant: PropTypes.oneOf(['primary', 'secondary', 'outline', 'text']),
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool,
+};
+
+export default Button;
--- src/pages/Home/Content/Discover/index.js
+++ src/pages/Home/Content/Discover/index.js
@@ -5,31 +5,33 @@ import Title from '~/components/Title';
 import { Grid, Row, Col } from '~/components/Grid/Grid.js';
 import { Link } from 'react-router-dom';
 import { SearchHomeIcon, CoinHomeIcon, UpHomeIcon } from '~/components/Icon';
+import Button from '~/components/Button';
+import CardDiscoverHouse from '~/components/CardDiscoverHouse';
 
 const cx = classNames.bind(styles);
 
-const ListItemsCard = [
-    {
-        icon: <SearchHomeIcon />,
-        title: 'Find out how much you can afford',
-        description: 'We’ll help you estimate your budget range. Save to your buyer profile to help in your search',
-        link: '/',
-    },
-    {
-        icon: <CoinHomeIcon />,
-        title: 'Understand your monthly costs',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin ipsum eumassa sollicitudin facilisis. ',
-        link: '/',
-    },
-    {
-        icon: <UpHomeIcon />,
-        title: 'Get help with your down payment',
-        description:
-            'In fermentum dignissim mauris et blandit. Fusce efficitur libero sit amet ullamcorper, nec volutpat justo fringilla',
-        link: '/',
-    },
-];
+// const ListItemsCard = [
+//     {
+//         icon: <SearchHomeIcon />,
+//         title: 'Find out how much you can afford',
+//         description: 'We’ll help you estimate your budget range. Save to your buyer profile to help in your search',
+//         link: '/',
+//     },
+//     {
+//         icon: <CoinHomeIcon />,
+//         title: 'Understand your monthly costs',
+//         description:
+//             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin ipsum eumassa sollicitudin facilisis. ',
+//         link: '/',
+//     },
+//     {
+//         icon: <UpHomeIcon />,
+//         title: 'Get help with your down payment',
+//         description:
+//             'In fermentum dignissim mauris et blandit. Fusce efficitur libero sit amet ullamcorper, nec volutpat justo fringilla',
+//         link: '/',
+//     },
+// ];
 
 function Discover() {
     return (
@@ -46,28 +48,50 @@ function Discover() {
 
                         <ul className={cx('list-btn')}>
                             <li>
-                                <button className={cx('btn-discover')}>Buying</button>
+                                <Button size="small" variant="outline">
+                                    <span className={cx('btn-text')}>Buying</span>
+                                </Button>
                             </li>
                             <li>
-                                <button className={cx('btn-discover')}>Rating</button>
+                                <Button size="small" variant="outline">
+                                    <span className={cx('btn-text')}>Rating</span>
+                                </Button>
                             </li>
                             <li>
-                                <button className={cx('btn-discover')}>Selling</button>
+                                <Button size="small" variant="outline">
+                                    <span className={cx('btn-text')}>Selling</span>
+                                </Button>
                             </li>
                         </ul>
 
-                        <div className={cx('list-items-card')}>
-                            {ListItemsCard.map((item, index) => (
-                                <div key={index} className={cx('item-card')}>
-                                    {item.icon}
-                                    <h4>{item.title}</h4>
-                                    <p>{item.description}</p>
-                                    <a href={item.link} className={cx('btn-link')}>
-                                        Learn more
-                                    </a>
-                                </div>
-                            ))}
-                        </div>
+                        <Grid wide>
+                            <Row>
+                                <Col sizeL={4} sizeM={12} sizeS={12}>
+                                    <CardDiscoverHouse
+                                        icon={<SearchHomeIcon />}
+                                        title="Find out how much you can afford"
+                                        description="We’ll help you estimate your budget range. Save to your buyer profile to help in your search"
+                                        to='/'
+                                    />
+                                </Col>
+                                <Col sizeL={4} sizeM={12} sizeS={12}>
+                                    <CardDiscoverHouse
+                                        icon={<CoinHomeIcon />}
+                                        title="Understand your monthly costs"
+                                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sollicitudin ipsum eumassa sollicitudin facilisis."
+                                        to='/'
+                                    />
+                                </Col>
+                                <Col sizeL={4} sizeM={12} sizeS={12}>
+                                    <CardDiscoverHouse
+                                        icon={<UpHomeIcon />}
+                                        title="Find out how much you can afford"
+                                        description="In fermentum dignissim mauris et blandit. Fusce efficitur libero sit amet ullamcorper, nec volutpat justo fringilla"
+                                        to='/'
+                                    />
+                                </Col>
+                            </Row>
+                        </Grid>
 
                         <div className={cx('discover-footer')}>
                             <p>Looking to spotlight a unique property with expert marketing?</p>
